73e43f11e3f2731239c78c479f4f9b3f77b34968
Armond <armonster@yandex.ru>
2021-10-28 22:20:16 +0300

    Add: HangmanClass, game logic

---
+++ hangman/hangman_game/HangmanClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace hangman_game
+{
+    class HangmanClass
+    {
+        string[] words;
+        string stringWord;
+        char[] charWord;
+        char[] viewWord;
+        int opennedLetters = 0;
+        Random random = new Random();
+        
+        public int WordLettersCount => stringWord.Length;
+        public bool IsSolved => opennedLetters == stringWord.Length;
+        public string ViewWord => new string(viewWord);
+
+        public HangmanClass(string path)
+        {
+            words = File.ReadAllLines(path);
+        }
+        public void GenerateWord()
+        {
+            stringWord = words[random.Next(0, words.Length)];
+            charWord = stringWord.ToCharArray();
+            viewWord = new char[stringWord.Length];
+
+            for (int i = 0; i < viewWord.Length; i++)
+            {
+                viewWord[i] = '*';
+            }
+        }
+        public bool CheckLetter(char letter)
+        {
+            bool isLetterExist = false;
+
+            for (int i = 0; i < charWord.Length; i++)
+            {
+                if (charWord[i] == letter)
+                {
+                    opennedLetters++;
+                    viewWord[i] = letter;
+                    charWord[i] = ' ';
+                    isLetterExist = true;
+                }
+            }
+            return isLetterExist;
+        }
+    }
+}
---
+++ hangman/hangman_game/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace hangman_game
+{
+    class Program
+    {
+        public static string path = @"C:\[UNITY]\hangman\word_rus.txt";
+        private const int max_errors = 10; 
+        static void Main(string[] args)
+        {
+                   
+            Console.WriteLine("Hello! Let's play game!");
+
+            HangmanClass word = new HangmanClass(path);
+
+            while(true)
+            {
+                word.GenerateWord();                
+                //создаем счетчик
+                int errors = max_errors;
+
+                Console.WriteLine($"Загадано слово из {word.WordLettersCount} букв. Отгадай его за {errors} попыток");
+
+                //цикл партии
+                while (errors > 0 && !word.IsSolved)
+                {
+                    Console.WriteLine("Введите букву");
+                    string inputString = Console.ReadLine();
+                    if(inputString.Length == 0 || !Char.IsLetter(inputString[0]))
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Вводи только буквы");
+                        continue;
+                    }
+                   
+
+                    Console.Clear();
+                    if(word.CheckLetter(inputString[0]))
+                    {
+                        Console.WriteLine("Угадал! Есть такая буква!");
+                    }
+                    else
+                    {
+                        errors--;
+                        Console.WriteLine($"Такой буквы нет! Осталось попыток: {errors}");
+                    }
+
+                    Console.WriteLine(word.ViewWord);
+                }
+                Console.WriteLine("Чтобы продожить нажми \"Ввод\"");
+                Console.ReadLine();
+                Console.Clear();
+
+            }
+        }
+    }
+}

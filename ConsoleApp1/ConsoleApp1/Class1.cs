911567a67392fb9ef4c5f1bfb82e4dbfc7421e9a
vova3118 <vova3118@gmail.com>
2021-10-31 18:45:53 +0300

    Lesson_6_Bad

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static string path = @"F:\Other\Unity\Projects\School\Hang_Man\word_rus.txt";
+        private const int maxErrors = 10;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello. Lets play game");
+
+            HangmanClass word = new HangmanClass(path);
+
+            while(true)
+            {
+                word.GenerateWord();
+                int errors = maxErrors;
+
+                Console.WriteLine($"Загадано слово из {word.WordLettersCount} букв. Отгадай его за {errors} попыток");
+
+                //цикл партии
+                while (errors > 0 && !word.IsSolved)
+                {
+                    Console.WriteLine("Введи букву");
+                    string inputString = Console.ReadLine();
+
+                    if (inputString.Length == 0 || !Char.IsLetter(inputString[0]))
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Только буквы!");
+                        continue;
+                    }
+                    
+
+                    Console.Clear();
+                    if(word.CheckLetter(inputString[0]))
+                    {
+                        Console.WriteLine("Угадал!");
+                    }
+                    else
+                    {
+                        errors--;
+                        Console.WriteLine($"Мимо! Осталось {errors} попыток");
+                    }
+
+                    Console.WriteLine(word.ViewWord);
+                }
+            }
+            Console.WriteLine("Конец");
+            Console.WriteLine("Жми \"ввод\" - продолжить");
+            Console.Read();
+            Console.Clear();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/HangmanClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class HangmanClass
+    {
+        string[] words;
+        string stringWord;
+        char[] charWord;
+        char[] viewWord;
+        int opennedLetters = 0;
+        Random random = new Random();
+
+        public int WordLettersCount => stringWord.Length;
+
+        public bool IsSolved => opennedLetters == stringWord.Length;
+
+        public string ViewWord => new string(viewWord);
+
+        public HangmanClass(string path)
+        {
+            words = File.ReadAllLines(path);
+        }
+
+        public void GenerateWord()
+        {
+            string stringWord = words[random.Next(0, words.Length)];
+            charWord = stringWord.ToCharArray();
+            viewWord = new char[stringWord.Length];
+            for (int i = 0; i < viewWord.Length; i++)
+            {
+                viewWord[i] = '*';
+            }
+        }
+        public bool CheckLetter(char letter)
+        {
+            bool isLetterExist = false;
+            for (int i = 0; i < charWord.Length; i++)
+            {
+                if (charWord[i] == letter)
+                {
+                    opennedLetters++;
+                    viewWord[i] = letter;
+                    charWord[i] = ' ';
+                    isLetterExist = true;
+                }
+            }
+            return isLetterExist;
+        }
+    }
+}

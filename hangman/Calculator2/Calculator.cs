bc81fc6de6f5ea838ada61ad7bd9d01dbce3fd7b
Armond <armonster@yandex.ru>
2021-11-01 21:34:58 +0300

    Add project "Calculator v2"

---
+++ hangman/Calculator2/Calculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+
+namespace Calculator2
+{
+    class Calculator
+    {
+        public double Calculate(string expression)
+        {
+            double result = double.NaN;
+            int operatorIndex = 0;
+            // проходим по выражению
+            for (int i = 0; i < expression.Length; i++)
+            {
+                char c = expression[i];
+                //проверяем оперции
+                if (c == '+' || c == '-')
+                {
+                    //записываем индекс
+                    operatorIndex = i;
+                    break;
+                }
+                else if ((c == '*' || c == '/') && operatorIndex == 0)
+                {
+                    operatorIndex = i;
+                }
+            }
+
+            if (operatorIndex == 0)
+            {
+                expression = expression.Trim();
+                //возвращаем число
+                result = double.Parse(expression);    
+            }
+            else
+            {
+                double first_val = Calculate(expression.Substring(0, operatorIndex));
+                double second_val = Calculate(expression.Substring(operatorIndex + 1));
+                switch (expression[operatorIndex])
+                {
+                    //вычисляем
+                    case '+':
+                        result = first_val + second_val;
+                        break;
+                    case '-':
+                        result = first_val - second_val;
+                        break;
+                    case '*':
+                        result = first_val * second_val;
+                        break;
+                    case '/':
+                        if(second_val != 0)
+                        {
+                            result = first_val / second_val;
+                        }
+                        break;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ hangman/Calculator2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Calculator2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool quit = false;
+            Console.WriteLine("Калькулятор v2 \n");
+
+            while (!quit)
+            {
+                Console.WriteLine("Введите выражение и нажмите Enter:");
+                string expression = Console.ReadLine();
+
+                Calculator p = new Calculator();
+                if (double.IsNaN(p.Calculate(expression)))
+                {
+                    Console.WriteLine("Ошибка в вычислениях! \n");
+                }
+                else
+                {
+                    Console.WriteLine($"Результат: {p.Calculate(expression)}\n");
+                }
+
+                Console.Write("Нажмите q для выхода или любой символ для продолжения ");
+                if (Console.ReadLine() == "q")
+                    quit = true;
+
+                Console.WriteLine("\n");
+            }
+            return;
+        }
+    }
+}

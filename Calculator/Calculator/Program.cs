7fe3e2ee9e2a27dba3ad26e48ff7d4ba8bef9369
SanKeeCheese <shura-19988@yandex.by>
2021-11-01 21:29:27 +0300

    no message

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -11,11 +11,36 @@ namespace Calculator
             Console.WriteLine("1.Калькулятор");
             Console.WriteLine("2.Достижения");
             int MenuChange = int.Parse(Console.ReadLine());
-
+            //Выбор вкладок меню
+            do
+            {
                 switch (MenuChange)
                 {
                     //Сам калькулятор
                     case 1:
+                        string LeftString = "", RightString = "", Solve = "";
+                        int z, answer;
+                        Console.Clear();
+                        Console.WriteLine("Введите пример со знаками без пробелов");
+                        string primer = (Console.ReadLine());
+                        int Dlina = primer.Length;
+                    //Решение примера
+                        for (int i = 1; i < Dlina; i++)
+                        {
+                            if (primer[i] == '*')
+                                {                               
+                                for (z = i; z != '*' || z != '/' || z != '-' || z != '+'; z--)
+                                    {
+                                    LeftString = primer[z] + LeftString;
+                                    }
+                                for (z = i; z != '*' || z != '/' || z != '-' || z != '+'; z++)
+                                {
+                                    RightString = RightString + primer[z];
+                                }
+                                Solve = Convert.ToString(int.Parse(LeftString) * int.Parse(RightString));
+                                
+                            }
+                        }
 
                         Console.Clear();
 
@@ -35,9 +60,7 @@ namespace Calculator
                     break;
                 }
             }
-
-            //Выбор вкладок меню
-            
+            while (MenuChange != 1 || MenuChange != 2);
         }
     }
 }

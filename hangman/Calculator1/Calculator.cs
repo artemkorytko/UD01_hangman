d98226c3b5069634801f40c596dfa392c6615858
Armond <armonster@yandex.ru>
2021-11-01 16:21:54 +0300

    Add project "Calculator v1"

---
+++ hangman/Calculator1/Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator1
+{
+    class Calculator
+    {
+        public static double Calculate(double a, double b, string operation)
+        {
+            double result = double.NaN;
+
+            switch (operation)
+            {
+                //сложение
+                case "+":
+                    result = a + b;
+                    break;
+                //вычитание
+                case "-":
+                    result = a - b;
+                    break;
+                //умножение
+                case "*":
+                    result = a * b;
+                    break;
+                //деление
+                case "/":
+                    // проверка деления на 0
+                    if (b != 0)
+                    {
+                        result = a / b;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
---
+++ hangman/Calculator1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Calculator1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool quit = false;
+            Console.WriteLine("Калькулятор v1 \n");
+
+            while (!quit)
+            {
+                Console.WriteLine("Введите число и нажмите Enter:");
+                string a = Console.ReadLine();
+
+                double double_a;
+                while (!double.TryParse(a, out double_a))
+                {
+                    Console.WriteLine("Не верно введено число");
+                    a = Console.ReadLine();
+                }
+
+                Console.WriteLine("Введите операцию и нажмите Enter:");
+                string operation = Console.ReadLine();
+
+                Console.WriteLine("Введите второе число и нажмите Enter:");
+                string b = Console.ReadLine();
+
+                double double_b;
+                while (!double.TryParse(b, out double_b))
+                {
+                    Console.WriteLine("Не верно введено число");
+                    b = Console.ReadLine();
+                }
+
+                try
+                {
+                    double result = Calculator.Calculate(double_a, double_b, operation);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("Ошибка в вычислениях! \n");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Результат: {result}\n");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Произошла ошибка: " + e.Message);
+                }
+
+                Console.Write("Нажмите q для выхода или любой символ для продолжения ");
+                if (Console.ReadLine() == "q") 
+                    quit = true;
+
+                Console.WriteLine("\n"); 
+            }
+            return;
+        }
+    }
+}

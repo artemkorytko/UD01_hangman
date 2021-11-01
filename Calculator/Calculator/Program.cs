ff8fabdd514b79669697ab0eac5454ea207a4c32
SanKeeCheese <shura-19988@yandex.by>
2021-11-01 22:36:45 +0300

    Calculator v0.3

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -9,19 +9,18 @@ namespace Calculator
             Console.WriteLine("Главное меню, выберите вкладку");
             System.Console.WriteLine();
             Console.WriteLine("1.Калькулятор");
-            Console.WriteLine("2.Достижения");
-            int MenuChange = int.Parse(Console.ReadLine());
+            int MenuChange = Convert.ToInt32(Console.ReadLine());
             //Выбор вкладок меню
             do
             {
                 switch (MenuChange)
-                {
+                {                  
                     //Сам калькулятор
                     case 1:
                         string LeftString = "", RightString = "", Solve = "";
-                        int z, answer;
+                        int z;
                         Console.Clear();
-                        Console.WriteLine("Введите пример со знаками без пробелов");
+                        Console.WriteLine("Введите пример со знаком без пробелов");
                         string primer = (Console.ReadLine());
                         int Dlina = primer.Length;
                     //Решение примера
@@ -29,25 +28,56 @@ namespace Calculator
                         {
                             if (primer[i] == '*')
                                 {                               
-                                for (z = i; z != '*' || z != '/' || z != '-' || z != '+'; z--)
+                                for (z = i-1; z != -1; z--)
                                     {
                                     LeftString = primer[z] + LeftString;
                                     }
-                                for (z = i; z != '*' || z != '/' || z != '-' || z != '+'; z++)
+                                for (z = i + 1; z != primer.Length; z++)
+                                {   
+                                    RightString = RightString + primer[z];
+                                }
+                                Solve = Convert.ToString(int.Parse(LeftString) * int.Parse(RightString));               
+                            }
+                            if (primer[i] == '-')
+                            {
+                                for (z = i - 1; z != -1; z--)
+                                {
+                                    LeftString = primer[z] + LeftString;
+                                }
+                                for (z = i + 1; z != primer.Length; z++)
                                 {
                                     RightString = RightString + primer[z];
                                 }
-                                Solve = Convert.ToString(int.Parse(LeftString) * int.Parse(RightString));
-                                
+                                Solve = Convert.ToString(int.Parse(LeftString) - int.Parse(RightString));
                             }
+                            if (primer[i] == '+')
+                            {
+                                for (z = i - 1; z != -1; z--)
+                                {
+                                    LeftString = primer[z] + LeftString;
+                                }
+                                for (z = i + 1; z != primer.Length; z++)
+                                {
+                                    RightString = RightString + primer[z];
+                                }
+                                Solve = Convert.ToString(int.Parse(LeftString) + int.Parse(RightString));
+                            }
+                            if (primer[i] == '/')
+                            {
+                                for (z = i - 1; z != -1; z--)
+                                {
+                                    LeftString = primer[z] + LeftString;
+                                }
+                                for (z = i + 1; z != primer.Length; z++)
+                                {
+                                    RightString = RightString + primer[z];
+                                }
+                                Solve = Convert.ToString(int.Parse(LeftString) / int.Parse(RightString));
+                            } 
                         }
-
+                        Console.WriteLine(Solve);
+                        Console.ReadKey();
                         Console.Clear();
-
-                        break;
-                    //Меню с достижениями
-                    case 2:
-
                         break;
                     //Если введена не та цифра
                     default:
@@ -55,8 +85,7 @@ namespace Calculator
                         Console.WriteLine("Главное меню, выберите вкладку");
                         System.Console.WriteLine();
                         Console.WriteLine("1.Калькулятор");
-                        Console.WriteLine("2.Достижения");
-                        MenuChange = int.Parse(Console.ReadLine());
+                        MenuChange = Console.Read();
                     break;
                 }
             }
